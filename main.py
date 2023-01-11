45a9b5751ff5404810d31c3d116e9a913bfbe85c
mac.window.10 <mac.window.10@gmail.com>
2023-01-11 20:07:56 +0500

    patient doctor

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def get_db_connection():
     return conn
 
 
+def get_doctors():
+    conn = get_db_connection()
+    doctors = conn.execute('SELECT * FROM users WHERE role="Doctor"').fetchall()
+    conn.close()
+    return doctors
+
+
 @app.route('/register', methods=['GET'])
 def register():
     return render_template('register.html')
@@ -18,6 +25,7 @@ def register():
 
 @app.route('/register', methods=['POST'])
 def register_post():
+    role = request.form['role']
     email = request.form['email']
     password = request.form['password']
     fname = request.form['fname']
@@ -30,6 +38,9 @@ def register_post():
     gender = request.form['gender']
     emergency_mobile = request.form['emergency_mobile']
     payment_mode = request.form['payment_mode']
+    qualification = request.form['qualification']
+    expertise = request.form['expertise']
+    pprint(role)
     pprint(email)
     pprint(password)
     pprint(fname)
@@ -37,11 +48,21 @@ def register_post():
     pprint(lname)
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO users (email, password, cnic, fname, mname, lname, postal_code, father_name, blood_group, gender, emergency_mobile, payment_mode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        (email, password, cnic, fname, mname, lname, postal_code, father_name, blood_group, gender, emergency_mobile,
-         payment_mode)
-    )
+    if role == 'Patient':
+        cur.execute(
+            "INSERT INTO users (role, email, password, cnic, fname, mname, lname, postal_code, father_name, blood_group, gender, emergency_mobile, payment_mode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (role, email, password, cnic, fname, mname, lname, postal_code, father_name, blood_group, gender,
+             emergency_mobile,
+             payment_mode)
+        )
+    else:
+        cur.execute(
+            "INSERT INTO users (role, email, password, cnic, fname, mname, lname, postal_code, father_name, blood_group, gender, emergency_mobile, payment_mode, qualification, expertise) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (
+                role, email, password, cnic, fname, mname, lname, postal_code, father_name, blood_group, gender,
+                emergency_mobile,
+                payment_mode, qualification, expertise)
+        )
     conn.commit()
     conn.close()
     return redirect(url_for('index', message='User registered successfully'))
@@ -63,15 +84,17 @@ def login_post():
     user = conn.execute('SELECT * FROM users WHERE email=? AND password=?',
                         (email, password)).fetchone()
     conn.close()
-    result = False
-    if user == None:
+    if user is None:
         pprint('user not found')
         message = 'Wrong email/password'
-    else:
-        pprint('user found')
-        message = 'User logged-in successfully'
-        session.__setitem__('login_status', True)
-    return redirect(url_for('index', message=message))
+        return redirect(url_for('login', message=message))
+
+    pprint('user found')
+    message = 'User logged-in successfully'
+    session.__setitem__('login_status', True)
+    session.__setitem__('role', user["role"])
+    session.__setitem__('email', user["email"])
+    return redirect(url_for('index', message=message, role=user["role"], email=user["email"]))
 
 
 @app.route('/logout')
@@ -85,7 +108,12 @@ def index():
     login_status = session.get('login_status')
     if login_status is None:
         return render_template('login.html')
-    return render_template('home.html')
+    role = session.get('role')
+    email = session.get('email')
+    if role == 'Patient':
+        doctors = get_doctors()
+        return render_template('home_patient.html', role=role, email=email, doctors=doctors)
+    return render_template('home_doctor.html', role=role, email=email)
 
 
 if __name__ == '__main__':
